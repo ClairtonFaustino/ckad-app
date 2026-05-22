35c695a4de4617c8ceb87e7e7db14d2a6a328d66
Clairton Faustino <clairton.faustino@somapay.com.br>
2026-05-22 15:52:15 -0300

    Update CI/CD configuration and add unit tests
    - Changed the CI/CD image to use Python 3.11-alpine for better compatibility with the application.
    - Enhanced the lint-and-test job to install dependencies and run unit tests using Pytest.
    - Introduced a new test file, test_app.py, containing unit tests for the Flask application, ensuring proper functionality of the home and health endpoints.

---
+++ test_app.py
@@ -0,0 +1,19 @@
+import pytest
+from app import app
+
+# Cria um cliente de testes do Flask
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"CKAD" in response.data
+
+def test_health(client):
+    response = client.get('/health')
+    assert response.status_code == 200
+    assert b"healthy" in response.data
